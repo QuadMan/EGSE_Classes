42ede708db2e3a3a237f7c2929d0f2f1b624b73c
QuadMan <shogunclub@gmail.com>
2013-11-25 23:05:01 +0400

    Убрал лишние функции, уменьшил размер кода

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FTD2XX_NET;
 
 namespace ConsoleApplication1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            FTDI ftdi = new FTDI();
+            ftdi.OpenBySerialNumber("test");
+            System.Console.WriteLine("ohohoh");
+            ftdi.Close();
+            System.Console.ReadLine();
         }
     }
 }
