135ef61950a2cfa318abf513faa33ae8d43a68d2
Ivan <korob@iki.rssi.ru>
2013-12-07 19:00:56 +0400

    Пример использования объявления event-ов

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,15 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
-using EGSE.UTILITES;
-using EGSE.Decoders.USB;
-using EGSE.Threading;
-using EGSE;
 using System.IO;
 using System.Collections.Specialized;
-//using EGSE;
-//using EGSE.Decoders;
 
+using EGSE.Utilites;
+using EGSE.Protocols;
+using EGSE.Threading;
+using EGSE;
 
 namespace ConsoleApplication1
 {
@@ -23,26 +21,18 @@ namespace ConsoleApplication1
             string hex = BitConverter.ToString(ba, 0, len);
             return hex.Replace("-", " ");
         }
-        static Device _dev;
+        //static Device _dev;
 
-        static void onDecMsg(MsgBase msg)
+        static void onDecMsg(ProtocolMsgEventArgs msg)
         {
-            USBProtocolMsg msg1 = msg as USBProtocolMsg;
-            if (msg1 != null)
-            {
                 //System.Console.WriteLine("addr={0}", msg1.addr);
-                //System.Console.WriteLine("addr={0} data={1}", msg1.addr, ByteArrayToString(msg1.data, msg1.dataLen));
-                //AManager.Run(msg as USBProtocolMsg);
-            }
-            //System.Console.WriteLine("errCnt={0}", _dec.errorsCount);
+                System.Console.WriteLine("addr={0} data={1}", msg.Addr, ByteArrayToString(msg.Data, msg.DataLen));
+                //AManager.Run(msg as USBProtocolMsg);  
         }
 
-        static void onErrMsg(MsgBase msg)
+        static void onErrMsg(ProtocolErrorEventArgs msg)
         {
-            USBProtocolErrorMsg msg1 = msg as USBProtocolErrorMsg;
-            if (msg1 != null) {
-                System.Console.WriteLine("msg={0} pos={2} data={1}", msg1.Msg, ByteArrayToString(msg1.data, msg1.dataLen), msg1.bufPos);
-            }
+            System.Console.WriteLine("msg={0} pos={2} data={1}", msg.Msg, ByteArrayToString(msg.Data, msg.DataLen), msg.ErrorPos);
         }
 
         static void fastDecode()
@@ -50,7 +40,7 @@ namespace ConsoleApplication1
 
         }
 
-        static void TMDecoder(USBProtocolMsg msg)
+        static void TMDecoder(ProtocolMsgEventArgs msg)
         {
             //Array.Copy(msg.data, raw, msg.data.Length);
             //fastDecode();
@@ -59,31 +49,31 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             /* Для проверки  декодера из файла или буфера */
-          //  Decoder5E4D dec = new Decoder5E4D();
-          //  dec.onMessage = onDecMsg;
-           // dec.onProtocolError = onErrMsg;
+            ProtocolUSB5E4D dec = new ProtocolUSB5E4D();
+            dec.GotProtocolMsg += new ProtocolUSBBase.ProtocolMsgEventHandler(onDecMsg);
+            dec.GotProtocolError += new ProtocolUSBBase.ProtocolErrorEventHandler(onErrMsg);
             //*/
-            //FileStream fStream = new FileStream(@"D:\Projects\USBLOG\Release\Win32\logs\201310\tmpBin.dat", FileMode.Open);
-            //DecoderThread dT = new DecoderThread(dec, fStream);
-
+            FileStream fStream = new FileStream(@"D:\Projects\USBLOG\Debug\Win32\logs\201310\21_161433.dat", FileMode.Open);
+            ProtocolThread dT = new ProtocolThread(dec, fStream);
             /* проверка работы модуля приема и декодирования данных */
-            FileStream fStream = new FileStream(@"D:\Projects\USBLOG\Release\Win32\logs\201310\usb_log.dat", FileMode.Create);
-            TextWriter fTxtWriter = new StreamWriter(@"D:\Projects\USBLOG\Release\Win32\logs\201310\usb_log.txt");
-            Decoder5E4D _dec = new Decoder5E4D(fStream, fTxtWriter, false, false);
-            _dec.onMessage = onDecMsg;
-            _dec.onProtocolError = onErrMsg;
-            _dev = new Device("FTVAFGPQ", _dec, new EGSE.USB.USBCfg(10));
+           // FileStream fStream = new FileStream(@"D:\Projects\USBLOG\Release\Win32\logs\201310\usb_log.dat", FileMode.Create);
+          //  TextWriter fTxtWriter = new StreamWriter(@"D:\Projects\USBLOG\Release\Win32\logs\201310\usb_log.txt");
+          //  ProtocolUSB5E4D _dec = new ProtocolUSB5E4D(fStream, fTxtWriter, false, false);
+          //  _dec.onMessage = onDecMsg;
+           // _dec.onProtocolError = onErrMsg;
+          //  _dev = new Device("FTVAFGPQ", _dec, new EGSE.USB.USBCfg(10));
           
             ///* выдаем команду в устройство */
             byte[] tmp = { 1 };
-            _dev.SendCmd(0x03, tmp);
+            byte[] tmpbuf;
+            dec.Encode(0x03, tmp, out tmpbuf);
             /* ждем пока не нажмем кнопку в консоли, тогда все завершаем */
             System.Console.ReadLine();
             /* текстовый лог загрываем */
-            _dev.finishAll();
-            fTxtWriter.Flush();
-            fTxtWriter.Close();
-            fStream.Close();
+          //  _dev.finishAll();
+          //  fTxtWriter.Flush();
+          //  fTxtWriter.Close();
+          //  fStream.Close();
         }
     }
 }
