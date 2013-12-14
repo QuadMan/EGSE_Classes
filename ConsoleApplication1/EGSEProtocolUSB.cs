38e770ec73d642e21848627c97f04ae5f0fe9192
Ivan <korob@iki.rssi.ru>
2013-12-14 17:17:23 +0400

    Рефактор согласно с правилами утилиты StyleCop 4.5. Отключенные правила: SA1650 (Validates that an element's documentation header contains correctly spelled words) SA1649 (Validates that a file header contains a file attribute which contains the name of the first type in the file) SA1402 (Validates that a C# document does not contain more than one class at the root level) SA1309 (Validates that the name of a field does not begin with an underscore) SA1126 (Verifies that calls to members are prefixed with the correct notation. In some case from sourcecode analysis we cannot be sure which prefix is required)

--- ConsoleApplication1/EGSEProtocolUSB.cs
+++ ConsoleApplication1/EGSEProtocolUSB.cs
@@ -1,52 +1,77 @@
-﻿/*** EDGE_Decoders_USB.cs
-**
-** (с) 2013 ИКИ РАН
- *
- * Базовый класс модуля протоколов USB и сообщений
-**
-** Author: Семенов Александр, Коробейщиков Иван
-** Project: КИА
-** Module: EDGE Decoders USB
-** Requires: 
-** Comments:
-**
-** History:
-**  0.1.0	(26.11.2013) - Начальная версия
- *  0.1.1   (27.11.2013) - Поменял заголовки функций, будем пользоваться передачей массивов
- *  0.1.2   (29.11.2013) - Выделил отдельный базовый класс протокола и сообщения протокола
-**
-*/
-
-using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="EGSEProtocolUSB.cs" company="IKI RSSI, laboratory №711">
+//     Copyright (c) IKI RSSI, laboratory №711. All rights reserved.
+// </copyright>
+// <author>Семенов Александр, Коробейщиков Иван</author>
+//-----------------------------------------------------------------------
 
 namespace EGSE.Protocols
 {
+    using System;
+
     /// <summary>
-    /// Базовый класс сообщения
-    /// TODO: возможно, его вывести выше в иерархии, и использовать как базу для всего
+    /// Базовый класс сообщения.
+    /// TODO: возможно, его вывести выше в иерархии, и использовать как базу для всего.
     /// </summary>
     public class MsgBase : EventArgs
     {
         /// <summary>
-        /// Данные сообщения
+        /// Данные сообщения.
         /// </summary>
-        public byte[] Data;
+        private byte[] _data;
+
         /// <summary>
-        /// Длина сообщения
+        /// Длина сообщения.
         /// </summary>
-        public int DataLen;
+        private int _dataLen;
+
+        /// <summary>
+        /// Gets or sets data message.
+        /// Считать или записать данные сообщения.
+        /// </summary>
+        public byte[] Data
+        {
+            get
+            {
+                return _data;
+            }
+
+            set
+            {
+                _data = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets длину сообщения.
+        /// </summary>
+        public int DataLen
+        {
+            get
+            {
+                return _dataLen;
+            }
+
+            set
+            {
+                _dataLen = value;
+            }
+        }
     }
+
     /// <summary>
-    /// Класс обмена сообщениями по протоколам USB
+    /// Класс обмена сообщениями по протоколам USB.
     /// </summary>
     public class ProtocolMsgEventArgs : MsgBase
     {
         /// <summary>
-        /// Адрес, по которому пришло сообщение
+        /// Адрес, по которому пришло сообщение.
         /// </summary>
-        public uint Addr;
+        private uint _addr;
+
         /// <summary>
-        /// Конструктор события: декодером обнаружено сообщение 
+        /// Initializes a new instance of the <see cref="ProtocolMsgEventArgs" /> class.
+        /// Конструктор события: декодером обнаружено сообщение.
         /// </summary>
         /// <param name="maxDataLen">Размер буфера</param>
         public ProtocolMsgEventArgs(uint maxDataLen)
@@ -55,7 +80,24 @@ namespace EGSE.Protocols
             DataLen = 0;
             Addr = 0;
         }
+
+        /// <summary>
+        /// Gets or sets адрес, по которому пришло сообщение.
+        /// </summary>
+        public uint Addr
+        {
+            get
+            {
+                return _addr;
+            }
+
+            set
+            {
+                _addr = value;
+            }
+        }
     }
+
     /// <summary>
     /// Класс ошибки кодера
     /// </summary>
@@ -64,12 +106,15 @@ namespace EGSE.Protocols
         /// <summary>
         /// Позиция ошибки в буфере
         /// </summary>
-        public uint ErrorPos;
+        private uint _errorPos;
+
         /// <summary>
         /// Признак ошибки
         /// </summary>
-        public string Msg;
+        private string _msg;
+
         /// <summary>
+        /// Initializes a new instance of the <see cref="ProtocolErrorEventArgs" /> class.
         /// Конструктор события: ошибка в кодере
         /// </summary>
         /// <param name="maxDataLen">Размер буфера</param>
@@ -79,22 +124,79 @@ namespace EGSE.Protocols
             DataLen = 0;
             ErrorPos = 0;
         }
+
+        /// <summary>
+        /// Gets or sets позиция ошибки в буфере
+        /// </summary>
+        public uint ErrorPos
+        {
+            get
+            {
+                return _errorPos;
+            }
+
+            set
+            {
+                _errorPos = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets признак ошибки
+        /// </summary>
+        public string Msg
+        {
+            get
+            {
+                return _msg;
+            }
+
+            set
+            {
+                _msg = value;
+            }
+        }
     }
+
     /// <summary>
     /// Абстрактный класс протокола USB
     /// </summary>
     public abstract class ProtocolUSBBase
     {
         /// <summary>
+        /// Объявление делегата обработки ошибок протокола
+        /// </summary>
+        /// <param name="e">Класс описывающий ошибку протокола</param>
+        public delegate void ProtocolErrorEventHandler(ProtocolErrorEventArgs e);
+
+        /// <summary>
+        /// Объявление делегата обработки сообщений протокола
+        /// </summary>
+        /// <param name="e">Класс описывающий сообщение протокола</param>
+        public delegate void ProtocolMsgEventHandler(ProtocolMsgEventArgs e);
+
+        /// <summary>
+        /// Объявление события: возникновение ошибки протокола в декодере
+        /// </summary>
+        public event ProtocolErrorEventHandler GotProtocolError;
+
+        /// <summary>
+        /// Объявление события: возникновение сообщения протокола в декодере
+        /// </summary>
+        public event ProtocolMsgEventHandler GotProtocolMsg;
+
+        /// <summary>
         /// Сброс конечного автомата состояния протокола в исходное состояние 
         /// </summary>
         public abstract void Reset();
+
         /// <summary>
         /// Метод декодирования данных
         /// </summary>
         /// <param name="buf">Буфер с данными для декодирования</param>
         /// <param name="bufLen">Размер буфера с данными</param>
         public abstract void Decode(byte[] buf, int bufLen);
+
         /// <summary>
         /// Метод кодирования данных
         /// Если функция выполняется с ошибкой, bufOut = null
@@ -108,47 +210,29 @@ namespace EGSE.Protocols
             bufOut = null;
             return false;
         }
-        /// <summary>
-        /// Объявление делегата обработки ошибок протокола
-        /// </summary>
-        /// <param name="e">Класс описывающий ошибку протокола</param>
-        public delegate void ProtocolErrorEventHandler(ProtocolErrorEventArgs e);
-        /// <summary>
-        /// Объявление события: возникновение ошибки протокола в декодере
-        /// </summary>
-        public event ProtocolErrorEventHandler GotProtocolError;
+
         /// <summary>
         /// Обертка события: возникновение ошибки протокола в декодере
         /// </summary>
         /// <param name="e">Класс описывающий ошибку протокола</param>
         protected virtual void OnProtocolError(ProtocolErrorEventArgs e)
         {
-            if (GotProtocolError != null)
+            if (this.GotProtocolError != null)
             {
-                GotProtocolError(e);
+                this.GotProtocolError(e);
             }
-
         }
-        /// <summary>
-        /// Объявление делегата обработки сообщений протокола
-        /// </summary>
-        /// <param name="e">Класс описывающий сообщение протокола</param>
-        public delegate void ProtocolMsgEventHandler(ProtocolMsgEventArgs e);
-        /// <summary>
-        /// Объявление события: возникновение сообщения протокола в декодере
-        /// </summary>
-        public event ProtocolMsgEventHandler GotProtocolMsg;
+
         /// <summary>
         /// Обертка события: возникновение сообщения протокола в декодере
         /// </summary>
         /// <param name="e">Класс описывающий сообщение протокола</param>
         protected virtual void OnProtocolMsg(ProtocolMsgEventArgs e)
         {
-            if (GotProtocolMsg != null)
+            if (this.GotProtocolMsg != null)
             {
-                GotProtocolMsg(e);
+                this.GotProtocolMsg(e);
             }
-
         }
     }
 }
