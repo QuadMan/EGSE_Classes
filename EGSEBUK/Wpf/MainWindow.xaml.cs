cd6463a97683330a9951505eeddf59661ce3989e
Ivan <shadow.bone.dark@gmail.com>
2014-02-19 18:57:31 +0400

    помарочка вышла

--- EGSEBUK/Wpf/MainWindow.xaml.cs
+++ EGSEBUK/Wpf/MainWindow.xaml.cs
@@ -252,13 +252,13 @@ namespace EGSE.Defaults
         }
 
         /// <summary>
-        /// Handles the Click event of the ControlSpaceWire control.
+        /// Handles the Click event of the ControlSpacewire control.
         /// </summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
-        private void ControlSpaceWire_Click(object sender, RoutedEventArgs e)
+        private void ControlSpacewire_Click(object sender, RoutedEventArgs e)
         {
-            if ((bool)ControlSpaceWire.IsChecked)
+            if ((bool)ControlSpacewire.IsChecked)
             {
                 winSimSpacewire.Show();
             }
